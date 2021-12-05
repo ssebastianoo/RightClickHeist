f5edc1c0a072772e028bb899afc340fab8f407ac
ssebastianoo <sebastianogirotto05@gmail.com>
2021-12-05 14:34:44 +0100

    discord webhook support

--- RightClickHeist.py
+++ RightClickHeist.py
@@ -1,23 +1,8 @@
-##Right Click Heist
-##Developed by NeonPinkQuartz for glasslibrary.xyz
+import requests, time, sys
 
-import requests
-import time
-
-##OpenSea demo
-# url = "https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=1"
-#
-# response = requests.request("GET", url)
-#
-# responselist = response.text.split(",")
-#
-# for item in responselist:
-#     if '"image_url"' in item and "null" not in item:
-#         print(item)
-#         break
-
-##Sample Return
-# https://storage.opensea.io/files/nonsense(.svg)
+webhook_url = None
+if len(sys.argv) > 1:
+    webhook_url = sys.argv[1]
 
 offset = 0
 
@@ -34,12 +19,15 @@ while True:
                 name = urlparts[-1]
                 print(url)
                 image = requests.get(url)
-                if "." not in name:
-                    with open(name + ".png", "wb") as myfile:
-                        myfile.write(image.content)
+                if webhook_url:
+                    requests.post(webhook_url, files={"file": ('nft.png', image.content)})
                 else:
-                    with open(name, "wb") as myfile:
-                        myfile.write(image.content)
+                    if "." not in name:
+                        with open(name + ".png", "wb") as myfile:
+                            myfile.write(image.content)
+                    else:
+                        with open(name, "wb") as myfile:
+                            myfile.write(image.content)
             break
     offset += 1
     time.sleep(.25)
